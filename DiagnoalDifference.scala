09ce6c323c8b37f082df36f6e82f414cd1243fd2
CCPStephanielv <stephanie@ccpgames.com>
2018-05-10 20:05:41 +0000

    dignoal difference

---
+++ DiagnoalDifference.scala
@@ -0,0 +1,31 @@
+object Solution {
+    def main(args: Array[String]) {
+        var n: Int= scala.io.StdIn.readInt()
+
+        //val rowArray = Array.ofDim[Int](n, n)
+        var lineArray = new Array[Int](n)
+        var strInput: String=""
+
+        var leftD: Int=0
+        var rightD: Int=0
+       
+        for(ir <- 0 until n) {
+            strInput =scala.io.StdIn.readLine()
+            lineArray = strInput.split(" ").map(_.toInt).take(n)
+
+            for(ic <- 0 until n) {
+                //rowArray(ir)(ic)=lineArray(ic)
+
+                if(ir == ic){
+                    leftD = leftD + lineArray(ic)
+                }
+
+                if(ir + ic == n-1){
+                    rightD = rightD + lineArray(ic)
+                }
+            }
+            
+        }
+        println(Math.abs(leftD - rightD))
+    }
+}
